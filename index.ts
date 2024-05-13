fc536c03eabe8b5f139c641a2a1fdf295f8f5628
Vladimir Khvan <vladimir.hvan@onlyoffice.com>
2024-05-13 22:56:52 +0500

    installed and configured development dependencies

---
+++ index.ts
@@ -0,0 +1,37 @@
+import fastify, { FastifyInstance, RouteShorthandOptions } from "fastify";
+import { Server, IncomingMessage, ServerResponse } from "http";
+
+const server: FastifyInstance<Server, IncomingMessage, ServerResponse> = fastify({});
+
+const opts: RouteShorthandOptions = {
+  schema: {
+    response: {
+      200: {
+        type: "object",
+        properties: {
+          pong: {
+            type: "string",
+          },
+        },
+      },
+    },
+  },
+};
+
+server.get("/ping", opts, async (request, reply) => {
+  return { pong: "it worked!" };
+});
+
+const start = async () => {
+  try {
+    await server.listen({ port: 3000 });
+
+    const address = server.server.address();
+    const port = typeof address === "string" ? address : address?.port;
+  } catch (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
